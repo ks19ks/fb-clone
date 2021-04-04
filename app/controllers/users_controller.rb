7c49de350bfc138e7bccfc9dccf983262c8791c5
kohei sakurai <ks19ks19ks19@gmail.com>
2021-04-04 17:29:21 +0900

    add user detail page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,8 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # something
-      
+      redirect_to user_path(@user.id)
     else
       render :new
     end
